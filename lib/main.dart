1b184adccd65fd9a85383f8e136c77ee992485ae
toyodahideyuki <toyodahideyuki4819@gmail.com>
2024-11-08 21:07:39 +0900

    commit 20241108 機能をdart側に移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'dart:html' as html;
+import 'dart:js' as js;
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,8 +13,29 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Web with JavaScript',
       home: Scaffold(
         appBar: AppBar(title: const Text('Virtual Background Example')),
-        body: const Center(child: Text('JavaScriptで機能を制御しています。')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(child: Text('JavaScriptで機能を制御しています。')),
+            ElevatedButton(
+              onPressed: () => html.window.location.href = 'camera.html',
+              child: const Text('カメラ画面へ'),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+class CameraControl {
+  static void initializeSegmentation() => js.context.callMethod('initializeSegmentation');
+
+  static void startVideoStream() => js.context.callMethod('startVideoStream');
+
+  static void stopVideoStream() => js.context.callMethod('stopVideoStream');
+
+  static void toggleCamera() => js.context.callMethod('toggleCamera');
+
+  static void changeBackground(String imagePath) => js.context.callMethod('changeBackground', [imagePath]);
 }
